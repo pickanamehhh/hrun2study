7cd4b9366123ee76d087035da01c6c543275df75
huhu <15576100991@163.com>
2021-03-23 00:09:51 +0800

    学习hook机制，skip忽略跳过用例

--- debugtalk.py
+++ debugtalk.py
@@ -6,5 +6,21 @@ def get_search_word():
     return word_list[num]
 
 
+def s():
+    print('测试前执行')
+
+
+def t():
+    print('测试后执行')
+
+
+def s1(test_name):
+    print('测试前执行[%s]' % test_name)
+
+
+def t1(test_name1):
+    print('测试后执行[%s]' % test_name1)
+
+
 if __name__ == '__main__':
     print(get_search_word())
