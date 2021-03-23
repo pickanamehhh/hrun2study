8d05d2ca6480342a4238212a780cc093efb1b3a0
huhu <15576100991@163.com>
2021-03-24 00:27:42 +0800

    学习skipIf、skipUnless忽略跳过用例

--- debugtalk.py
+++ debugtalk.py
@@ -1,8 +1,10 @@
 import random
+import requests
+
 
 def get_search_word():
     word_list = ['lol', 'csgo', 'pubg', '守望先锋']
-    num = random.randint(0, len(word_list)-1)
+    num = random.randint(0, len(word_list) - 1)
     return word_list[num]
 
 
@@ -22,5 +24,23 @@ def t1(test_name1):
     print('测试后执行[%s]' % test_name1)
 
 
+def get_true():
+    return True
+
+
+def get_access_token():
+    params = {
+        'grant_type': 'client_credential',
+        'appid': 'wx8708f96abc9abaf3',
+        'secret': '5be760a8fac19282515d47d326ab108b'
+    }
+    try:
+        response = requests.get(url='https://api.weixin.qq.com/cgi-bin/token', params=params)
+        token = response.json()['assess_token']
+    except KeyError as e:
+        return None
+    return token
+
+
 if __name__ == '__main__':
-    print(get_search_word())
+    print(get_access_token())
